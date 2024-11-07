9758f6fd09fa72c3da381f918cc3fe68c7d49367
sujal-pawar <sujalpawar00007@gmail.com>
2024-11-07 08:32:51 +0530

    Added selection sort (input)

--- Selection-sort.c
+++ Selection-sort.c
@@ -1,35 +1,47 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 
-void swap(int *x,int *y){
+void swap(int *x, int *y)
+{
     int temp = *x;
-    *x=*y;
-    *y=temp;
+    *x = *y;
+    *y = temp;
 }
 
-void selection(int A[],int n){
-    int i,j,k;
-    for (i=0;i<n-1;i++){
-        for(j=k=i;j<n;j++){
-            if(A[j]<A[k]){
-                k=j;
+void selection(int A[], int n)
+{
+    int i, j, k;
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = k = i; j < n; j++)
+        {
+            if (A[j] < A[k])
+            {
+                k = j;
             }
         }
-        swap(&A[i],&A[k]);
+        swap(&A[i], &A[k]);
     }
 }
 
-int main(){
+int main()
+{
 
-    int A[]={2,6,3,7,4,5,8,11,23};
-    int n = sizeof(A)/sizeof(A[0]);
+    int A[] = {2, 6, 3, 7, 4, 5, 8, 11, 23};
+    int n = sizeof(A) / sizeof(A[0]);
 
-    selection(A,n);
+    // Custom Input :
+    // for(int i=0; i<n; i++)
+    // {
+    //     scanf("%d",&A[i]);
+    // }
+
+    selection(A, n);
 
     for (int i = 0; i < n; i++)
     {
-        printf(" %d",A[i]);
+        printf(" %d", A[i]);
     }
-    
+
     return 0;
 }
