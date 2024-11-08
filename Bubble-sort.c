c6c09ffea4b7db3db4dde0727c6c4b833fb3fb0d
sujal-pawar <sujalpawar00007@gmail.com>
2024-11-08 11:22:49 +0530

    Added Quick sort

--- Bubble-sort.c
+++ Bubble-sort.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 // Bubble Sort
+
 int swap(int *x, int *y)
 {
     int temp = *x;
---
+++ Quick-sort.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int partition(int A[], int l, int h)
+{
+    int i, j, pivot;
+    pivot = A[l];
+    i = l;
+    j = h+1;
+    do
+    {
+        do{i++;} while (A[i] <= pivot && i<=h);
+        do{j--;} while (A[j] > pivot && j>=l);
+        if (i < j) swap(&A[i], &A[j]);
+    } while (i < j);
+
+        swap(&A[l],&A[j]);
+        return j;
+}
+
+void quickSort(int A[], int l, int h){
+    if(l<h){
+       int j = partition(A,l,h);
+       quickSort(A,l,j-1);
+       quickSort(A,j+1,h);
+    }
+}
+
+int main()
+{
+
+    int A[] = {3, 2, 5, 4, 1, 9, 6, 8, 11, 1};
+    int n = sizeof(A) / sizeof(A[0]);
+
+    // Custom Input
+    // for (int i=0;i<n;i++){
+    //     scanf("%d",&A[i]);
+    // }
+
+    quickSort(A, 0, n-1);
+
+    printf("Sorted Array :");
+    for (int i = 0; i < n; i++)
+    {
+        printf(" %d", A[i]);
+    }
+    return 0;
+}
