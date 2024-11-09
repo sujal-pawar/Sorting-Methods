67272bb2a1d188d7fb84bd5c5ca0e3a279d0b204
sujal-pawar <sujalpawar00007@gmail.com>
2024-11-10 02:32:33 +0530

    Iterative Merge sort

--- Merge-sort.c
+++ Merge-sort.c
@@ -16,13 +16,30 @@ void Merge(int A[], int l, int mid, int h)
         }
     }
 
-    for (; i <= mid; i++)
-        B[k++] = A[i];
-    for (; j <= h; j++)
-        B[k++] = A[j];
+    while (i <= mid)
+        B[k++] = A[i++];
+    while (j <= h)
+        B[k++] = A[j++];
 
-    for (i = 0; i < h; i++)
+    for (i = l; i <= h; i++)
         A[i] = B[i];
+}
+
+void ImergeSort(int A[],int n){
+    
+    int p,l,mid,h,i;
+    for( p=2 ; p<=n ; p=p*2 ) // log(n) iterations / Passes
+    {
+        for( i=0 ; i+p-1<n ; i=i+p ){
+            l=i;
+            h=i+p-1;
+            mid=(l+h)/2;
+            Merge(A,l,mid,h);
+        }
+    }
+    if(p/2<n) // For elements that don't form a complete pair.
+        Merge(A,0,(p/2)-1,n-1);
+
 }
 
 int main()
@@ -30,6 +47,13 @@ int main()
 
     int A[] = {3, 7, 1, 6, 8, 9, 11, 10, 15, 12};
     int n = sizeof(A) / sizeof(A[0]);
+
+    ImergeSort(A,n);
+
+    printf("Sorted Array :");
+    for(int i=0;i<n;i++){
+        printf(" %d",A[i]);
+    }
 
     return 0;
 }
